0570db2bd7f2fa71467cd9fedab9d8f51eb73847
hagleyson <hagleyson@gmail.com>
2022-01-11 12:20:14 -0300

    using useParams

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Products from "./pages/products";
 import Welcome from "./pages/Welcome";
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <>
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route path="/products">
           <Products />
+        </Route>
+        <Route path="/products-detail/:productId">
+          <ProductDetail />
         </Route>
       </main>
     </>
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,15 @@
 const Products = () => {
-  return <h1>Products</h1>;
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>A Book</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
